19cf40ac8aca671397e2dd180818b510f1635756
GrawtoG <zsuetam.k@gmail.com>
2026-01-13 14:16:19 +0100

    init

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Matieusz on 13.01.2026.
+//
+
+#include <stdio.h>
+#include <iostream>
+using std::cout, std::endl;
+int main(){
+
+    cout<<"Hello world!!!"<<endl;
+    return 0;
+}
