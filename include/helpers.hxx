1adacc78dae2f67777b27d90e87f7eadbb23ffdd
KjiAGH <kji@student.agh.edu.pl>
2026-02-03 22:41:12 +0100

    Update helpers.hxx

--- include/helpers.hxx
+++ include/helpers.hxx
@@ -1,16 +1,10 @@
-#ifndef HELPERS_HPP_
-#define HELPERS_HPP_
-
+#ifndef HELPERS_HXX
+#define HELPERS_HXX
 #include <functional>
 #include <random>
 
-#include "types.hxx"
-
-extern std::random_device rd;
-extern std::mt19937 rng;
-
-extern double default_probability_generator();
-
+using ProbabilityGenerator = std::function<double()>;
 extern ProbabilityGenerator probability_generator;
+double default_probability_generator();
+#endif
 
-#endif /* HELPERS_HPP_ */
